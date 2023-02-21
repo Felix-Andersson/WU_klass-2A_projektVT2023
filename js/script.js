e8be609d64c24d5dbd4c002e0f6f625eeeea0587
Felix-Andersson <felix.andersson3@elev.ga.ntig.se>
2023-02-21 09:14:04 +0100

    Gridlayout

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const arrow = document.getElementById('arrow')
+const nav = document.getElementById('nav-links')
+
+function toggleMenu() {
+    arrow.classList.toggle('arrow_toggle')
+    nav.classList.toggle('nav_toggle')
+}
+
+arrow.addEventListener('click', toggleMenu)
