089be079c557a052271b42501f759da0ec767409
Felix-Andersson <felix.andersson3@elev.ga.ntig.se>
2023-03-20 12:58:31 +0100

    quality of life

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
-const arrow = document.getElementById('arrow')
-const nav = document.getElementById('nav_links')
+const arrow = document.getElementById('arrow');
+const nav = document.getElementById('nav_links');
+const field = document.querySelector('input[type="text"]');
+const list = document.getElementById('list');
 
 function toggleMenu() {
-    arrow.classList.toggle('arrow_toggle')
-    nav.classList.toggle('nav_toggle')
+    arrow.classList.toggle('arrow_toggle');
+    nav.classList.toggle('nav_toggle');
+}
+function toggleList() {
+    list.classList.toggle('list_toggle');
 }
 
-arrow.addEventListener('click', toggleMenu)
+arrow.addEventListener('click', toggleMenu);
+field.addEventListener('focus', toggleList);
+field.addEventListener('blur', toggleList);
+
+function search_type() {
+    let input = document.getElementById('searchbar').value.toLowerCase();
+    let types = document.getElementsByClassName('types');
+    
+    for (i = 0; i < types.length; i++) { 
+        if (!types[i].innerHTML.toLowerCase().includes(input)) {
+            types[i].style.display="none";
+        }
+        else {
+            types[i].style.display="list-item";
+        }
+    }
+}
+
